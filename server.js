b51bac9d2694170a7a6d5bbfb721791fa63f6ad1
MOHAMED ABDELRAHMAN <mohammed.abdulrahman@apryse.com>
2024-09-05 12:43:00 +0300

    Adding of eslint.
    1. Adding of eslint warning disables to app.js
    2. Installing of eslint dependencies to the project.
    3. Adding of eslint.config.mjs.

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 // modified original source from https://www.npmjs.com/package/reload#using-reload-in-express
 
-var express = require('express')
-var http = require('http')
-var path = require('path')
-var bodyParser = require('body-parser')
-var logger = require('morgan')
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-var app = express()
+var app = express();
 
-var appDir = path.join(__dirname, 'app')
+var appDir = path.join(__dirname, 'app');
 
-app.set('port', process.env.PORT || 3000)
-app.use(logger('dev'))
-app.use(bodyParser.json()) // Parses json, multi-part (file), url-encoded
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json()); // Parses json, multi-part (file), url-encoded
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(appDir, 'index.html'))
-})
+  res.sendFile(path.join(appDir, 'index.html'));
+});
 
 app.use(express.static(appDir));
 
-var server = http.createServer(app)
+var server = http.createServer(app);
 
 server.listen(app.get('port'), function () {
-  console.log('Web server listening on port ' + app.get('port'))
-})
+  console.log('Web server listening on port ' + app.get('port'));
+});
